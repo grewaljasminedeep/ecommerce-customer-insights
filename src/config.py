280636b18d7bc0f0a69980ff8b6a233994f12d60
Jasmine <grewaljasminedeep@gmail.com>
2026-08-12 12:59:49 -0400

    ..

--- src/config.py
+++ src/config.py
@@ -8,3 +8,12 @@ def load_config(path="config/config.yaml"):
     with open(path, "r", encoding="utf-8") as f:
         cfg = yaml.safe_load(f)
     cfg["mysql"]["host"] = os.getenv("MYSQL_HOST", cfg["mysql"]["host"])
+    cfg["mysql"]["port"] = int(os.getenv("MYSQL_PORT", cfg["mysql"]["port"]))
+    cfg["mysql"]["user"] = os.getenv("MYSQL_USER", cfg["mysql"]["user"])
+    cfg["mysql"]["password"] = os.getenv("MYSQL_PASSWORD", cfg["mysql"]["password"])
+    cfg["mysql"]["database"] = os.getenv("MYSQL_DATABASE", cfg["mysql"]["database"])
+    cfg["neo4j"]["uri"] = os.getenv("NEO4J_URI", cfg["neo4j"]["uri"])
+    cfg["neo4j"]["user"] = os.getenv("NEO4J_USER", cfg["neo4j"]["user"])
+    cfg["neo4j"]["password"] = os.getenv("NEO4J_PASSWORD", cfg["neo4j"]["password"])
+    cfg["pipeline"]["export_dir"] = os.getenv("EXPORT_DIR", cfg["pipeline"]["export_dir"])
+    return cfg
