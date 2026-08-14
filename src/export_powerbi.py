67fdc3a7987cfa9e5f0d6ba0e3dbacad60d82d1e
Jasmine <grewaljasminedeep@gmail.com>
2026-08-13 20:19:19 -0400

    ..

--- src/export_powerbi.py
+++ src/export_powerbi.py
@@ -8,3 +8,7 @@ def export_df(df, path):
     df.to_csv(path, index=False)
 
 def export_all(export_dir, category_df, clv_df, affinity_df):
+    ensure_dir(export_dir)
+    export_df(category_df, os.path.join(export_dir, "category_revenue.csv"))
+    export_df(clv_df, os.path.join(export_dir, "customer_clv.csv"))
+    export_df(affinity_df, os.path.join(export_dir, "product_affinity.csv"))
