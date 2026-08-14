336ad3ba543b3104eb38e2366352920556540a7e
Jasmine <grewaljasminedeep@gmail.com>
2026-08-13 20:42:53 -0400

    ..

--- src/main.py
+++ src/main.py
@@ -8,3 +8,13 @@ from src.neo4j_graph import connect_neo4j, upsert_graph
 def main():
     cfg = load_config()
     mysql_conn = connect_mysql(cfg["mysql"])
+    users_df = fetch_users_df(mysql_conn)
+    products_df = fetch_products_df(mysql_conn)
+    orders_df = fetch_orders_df(mysql_conn)
+    pairs_df = build_copurchase_pairs(orders_df)
+    category_df = compute_category_revenue(orders_df, products_df)
+    clv_df = compute_clv_by_user(orders_df)
+    affinity_df = compute_product_affinity_score(pairs_df)
+    export_all(cfg["pipeline"] ["export_dir"], category_df, clv_df, affinity_df)
+    neo4j_driver = connect_neo4j(cfg["neo4j"])
+    upsert_graph(neo4j_driver, users_df, products_df, orders_df)
